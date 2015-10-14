c38912f11b86a58922df3ebe6c0cc318fe8c6534
amchercashin <amchercashin@gmail.com>
2015-10-14 23:22:39 +0300

    first glmnet gaussian model on complete cases

--- beeline.R
+++ beeline.R
@@ -1,7 +1,7 @@
 #library(readr)
 library(caret)
 library(glmnet)
-
+library(mice)
 # READ COMPETITION DATA:
 {
 train <- read.csv("./data/train.csv", na.strings = c("NA", ""))
@@ -12,9 +12,11 @@ train$x6 <- as.factor(train$x6); train$x7 <- as.factor(train$x7); train$x34 <- a
 test$x6 <- as.factor(test$x6); test$x7 <- as.factor(test$x7); test$x34 <- as.factor(test$x34)
 feature_names <- names(train)[1:(ncol(train)-1)]
 
-in_train <- sample(nrow(train), 30000)
+train_no_na <- train[complete.cases(train),]
 
-system.time(X <- model.matrix(~., train[in_train, feature_names])[,-1])
+in_train <- sample(nrow(train_no_na), 20000)
 
-system.time(lasso_cv <- cv.glmnet(X, train$y[in_train], family = "gaussian", alpha = 1, 
+system.time(X <- model.matrix(~., train_no_na[in_train, feature_names])[,-1])
+
+system.time(lasso_cv <- cv.glmnet(X, train_no_na$y[in_train], family = "gaussian", alpha = 1, 
                                   type.measure = "auc", parallel = TRUE)) #27
