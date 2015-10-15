8e0c7c1b37dc87dd8ff7104a61c0cbdfebfeb021
amchercashin <amchercashin@gmail.com>
2015-10-15 09:55:25 +0300

    make predictions, not good

--- beeline.R
+++ beeline.R
@@ -20,3 +20,15 @@ system.time(X <- model.matrix(~., train_no_na[in_train, feature_names])[,-1])
 
 system.time(lasso_cv <- cv.glmnet(X, train_no_na$y[in_train], family = "gaussian", alpha = 1, 
                                   type.measure = "auc", parallel = TRUE)) #27
+bestLambda <- lasso_cv$lambda.min
+bestLambdaCol <- which(lasso_cv$lambda==lasso_cv$lambda.min)
+lassoImpVars <- names(which(lasso_cv$glmnet.fit$beta[,bestLambdaCol]!=0))
+lassoImpVars <- ifelse(nchar(lassoImpVars) > 10, substr(lassoImpVars, 1, nchar(lassoImpVars)-10), lassoImpVars)
+lassoImpVars <- unique(lassoImpVars)
+
+new_x <- model.matrix(~., train_no_na[-in_train, feature_names])[,-1]
+new_y <- predict(lasso_cv, new_x, s="lambda.min", type = "response")
+new_y <- ifelse(new_y<1, 1, new_y)
+new_y <- round(new_y)
+new_y <- factor(new_y)
+confusionMatrix(new_y, train_no_na[-in_train, "y"])
