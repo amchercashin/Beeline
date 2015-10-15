2a2af2a783b476c4ecf047b6439b2206517e011a
amchercashin <amchercashin@gmail.com>
2015-10-15 12:28:20 +0300

    trying h2o

--- beeline.R
+++ beeline.R
@@ -1,6 +1,6 @@
 #library(readr)
 library(caret)
-library(glmnet)
+library(h2o)
 library(mice)
 # READ COMPETITION DATA:
 {
@@ -16,15 +16,34 @@ train_no_na <- train[complete.cases(train),]
 
 in_train <- sample(nrow(train_no_na), 20000)
 
-system.time(X <- model.matrix(~., train_no_na[in_train, feature_names])[,-1])
+## Start a local cluster with 2GB RAM
+localH2O = h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, 
+                    Xmx = '3g')
+## 
+train_h2o <- as.h2o(localH2O, train_no_na[in_train, ], key = 'train')
+validation_h2o <- as.h2o(localH2O, train_no_na[-in_train, ], key = 'validation')
 
-system.time(lasso_cv <- cv.glmnet(X, train_no_na$y[in_train], family = "gaussian", alpha = 1, 
-                                  type.measure = "auc", parallel = TRUE)) #27
-bestLambda <- lasso_cv$lambda.min
-bestLambdaCol <- which(lasso_cv$lambda==lasso_cv$lambda.min)
-lassoImpVars <- names(which(lasso_cv$glmnet.fit$beta[,bestLambdaCol]!=0))
-lassoImpVars <- ifelse(nchar(lassoImpVars) > 10, substr(lassoImpVars, 1, nchar(lassoImpVars)-10), lassoImpVars)
-lassoImpVars <- unique(lassoImpVars)
+system.time(
+model <-        h2o.deeplearning(x = 1:62,  # column numbers for predictors
+                                 y = 63,   # column number for label
+                                 training_frame = train_h2o, # data in H2O format
+                                 validation_frame = validation_h2o,
+                                 activation = "TanhWithDropout", # or 'Tanh'
+                                 input_dropout_ratio = 0.2, # % of inputs dropout
+                                 hidden_dropout_ratios = c(0.5,0.5,0.5), # % for nodes dropout
+                                 balance_classes = TRUE, 
+                                 hidden = c(50,50,50), # three layers of 50 nodes
+                                 epochs = 10) # max. no. of epochs
+)
+# system.time(X <- model.matrix(~., train_no_na[in_train, feature_names])[,-1])
+
+# system.time(lasso_cv <- cv.glmnet(X, train_no_na$y[in_train], family = "gaussian", alpha = 1, 
+#                                   type.measure = "auc", parallel = TRUE)) #27
+# bestLambda <- lasso_cv$lambda.min
+# bestLambdaCol <- which(lasso_cv$lambda==lasso_cv$lambda.min)
+# lassoImpVars <- names(which(lasso_cv$glmnet.fit$beta[,bestLambdaCol]!=0))
+# lassoImpVars <- ifelse(nchar(lassoImpVars) > 10, substr(lassoImpVars, 1, nchar(lassoImpVars)-10), lassoImpVars)
+# lassoImpVars <- unique(lassoImpVars)
 
 new_x <- model.matrix(~., train_no_na[-in_train, feature_names])[,-1]
 new_y <- predict(lasso_cv, new_x, s="lambda.min", type = "response")
